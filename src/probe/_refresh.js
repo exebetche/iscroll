89255e9ee8e738ce20dec7a207b4fd8b639bef54
exebetche <guillaume.lemaout@gmail.com>
2017-02-05 18:46:02 +0100

    virtual list

---
+++ src/probe/_refresh.js
@@ -0,0 +1,6 @@
+
+		this.scrollerWidth	= rect.width + this.widthShift;
+		this.scrollerHeight	= rect.height + this.heightShift;
+
+		this.maxScrollX		= this.wrapperWidth - this.scrollerWidth;
+		this.maxScrollY		= this.wrapperHeight - this.scrollerHeight;
