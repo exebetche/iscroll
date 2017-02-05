d427507fa6e5a144d41c49462f5526563ca47fc9
exebetche <guillaume.lemaout@gmail.com>
2017-02-05 18:42:51 +0100

    Update probe.js

--- src/probe/probe.js
+++ src/probe/probe.js
@@ -3,4 +3,8 @@ iScroll.prototype._initProbe = function () {
 	if ( this.options.probeType == 3 ) {
 		this.options.useTransition = false;
 	}
+	
+	// Shifts for virtual list
+	this.widthShift = 0;
+	this.heightShift = 0;
 };
